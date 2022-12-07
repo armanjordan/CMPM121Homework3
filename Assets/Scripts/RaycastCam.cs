bad2a8893ea6950c6248c180307a5d2842fff8bf
CallMeCalvin101 <calvinkurniadjaja@gmail.com>
2022-12-07 13:11:18 -0800

    improvements to interaction

--- Assets/Scripts/RaycastCam.cs
+++ Assets/Scripts/RaycastCam.cs
@@ -5,17 +5,26 @@
  {
      Ray ray;
      RaycastHit hit;
+     public static int numItems;
 
      float maxDistance = 100;
      private int layerMask = 1 << 6;
+
+     void Start() 
+     {
+        numItems = 0;
+     }
      
      void Update()
      {
          ray = Camera.main.ScreenPointToRay(Input.mousePosition);
          if(Physics.Raycast(ray, out hit, maxDistance, layerMask))
          {
-            if(Input.GetMouseButtonDown(0)) {
-                print(hit.collider.name);
+            if(Input.GetMouseButtonDown(0)) 
+            {
+                numItems += 1;
+                print(numItems);
+                Destroy(hit.collider);
             }
          }
      }
