aa3e984302f5112174619606824559af284f39e1
Dilnos521 <ddelacer@ucsc.edu>
2022-12-06 21:56:12 -0800

    Spot Light Made and Light Flicker script
    Set up a spot light connected to the character's camera to allow it to move along while looking around. Also made a script for the light flickering, though at the moment it might be nonfunctional (it seems to take a long time to load, and my task manager keeps marking as "not responding" when I check on it to see if something's up) and as such isn't attached to the light at the moment.

---
+++ Assets/Scripts/FlickerLight.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlickerLight : MonoBehaviour
+{
+    private float minFlickerSpeed = 0.1f;
+    private float maxFlickerSpeed = 1.0f;
+    private float timer;
+    private float waitTimeSeconds = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(GetComponent<Light>().enabled is false)
+        {
+            GetComponent<Light>().enabled = true;
+            timer = 0.0f;
+            waitTimeSeconds = Random.Range(minFlickerSpeed, maxFlickerSpeed);
+            do
+            {
+                timer =+ 0.1f;
+            }while(timer < waitTimeSeconds);
+        }
+        else
+        {
+            GetComponent<Light>().enabled = false;
+            timer = 0.0f;
+            waitTimeSeconds = Random.Range(minFlickerSpeed, maxFlickerSpeed);
+            do
+            {
+                timer =+ 0.1f;
+            }while(timer < waitTimeSeconds);
+        }
+    }
+}
