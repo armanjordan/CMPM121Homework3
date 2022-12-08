7ec467cf866ac8a0d9a51b4f53a9d8c48c52b1bd
CallMeCalvin101 <calvinkurniadjaja@gmail.com>
2022-12-07 17:20:31 -0800

    particle system click remove

--- Assets/Scripts/RaycastCam.cs
+++ Assets/Scripts/RaycastCam.cs
@@ -3,37 +3,49 @@
  
  public class RaycastCam : MonoBehaviour 
  {
-     Ray ray;
-     RaycastHit hit;
-     public static int numItems;
+    // Variables to track raycast
+    Ray ray;
+    RaycastHit hit;
 
-     float maxDistance = 100;
-     private int layerMask = 1 << 6;
+    float maxDistance = 6;
+    private int layerMask = 1 << 6;
 
-     [SerializeField]
-     private GameObject particles;
+    // Global tracking of Objects clicked
+    public static int numItems;
 
-     void Start() 
-     {
+    // GameObject to display after all clues been clicked
+    [SerializeField]
+    private GameObject payoff;
+
+    // Variable to track the particles attached to the interactables
+    private ParticleSystem objParticles;
+
+    // Initialize values
+    void Start() 
+    {
         numItems = 0;
-        particles.SetActive(false);
-     }
-     
-     void Update()
-     {
-         ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-         if(Physics.Raycast(ray, out hit, maxDistance, layerMask))
-         {
+        payoff.SetActive(false);
+    }
+    
+
+    void Update()
+    {
+        // Grabs current ray
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if(Physics.Raycast(ray, out hit, maxDistance, layerMask))
+        {
             if(Input.GetMouseButtonDown(0)) 
             {
                 numItems += 1;
                 print(numItems);
+                objParticles = hit.transform.GetComponent<ParticleSystem>();
+                objParticles.Stop(false);
                 Destroy(hit.collider);
             }
-         }
+        }
 
-         if (numItems >=5 ) {
-            particles.SetActive(true);
-         }
-     }
- }
+        if (numItems >= 5) {
+            payoff.SetActive(true);
+        }
+    }
+}
